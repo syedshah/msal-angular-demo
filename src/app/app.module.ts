d5738768e3e5a5a53ddbc5d34a00d7670592d9b0
Syed Shah <Syed.Shah@lancashiregroup.com>
2023-10-06 10:31:52 +0100

    MsalInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalBroadcastService, MsalGuard, MsalGuardConfiguration, MsalRedirectComponent, MsalService } from '@azure/msal-angular';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalBroadcastService, MsalGuard, MsalGuardConfiguration, MsalInterceptor, MsalInterceptorConfiguration, MsalRedirectComponent, MsalService, ProtectedResourceScopes } from '@azure/msal-angular';
 import { IPublicClientApplication, InteractionType, LogLevel, PublicClientApplication } from '@azure/msal-browser';
 
 
@@ -48,6 +48,19 @@ function MsalGuardConfigFactory(): MsalGuardConfiguration{
     }
 }
 
+function MsalInterceptorConfigFacory(): MsalInterceptorConfiguration{
+    const myProtectedResourcesMap = new Map<string, Array<string | ProtectedResourceScopes> | null>();
+    myProtectedResourcesMap.set('https://graph.microsoft.com/v1.0/me', [{
+        httpMethod: 'GET',
+        scopes: ['user.read']
+    }]);
+
+    return{
+        interactionType: InteractionType.Popup,
+        protectedResourceMap: myProtectedResourcesMap
+    }
+}
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -73,6 +86,15 @@ function MsalGuardConfigFactory(): MsalGuardConfiguration{
         {
             provide: MSAL_GUARD_CONFIG,
             useFactory: MsalGuardConfigFactory
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: MsalInterceptor,
+            multi: true
+        },
+        {
+            provide: MSAL_INTERCEPTOR_CONFIG,
+            useFactory: MsalInterceptorConfigFacory
         },
         MsalService,
         MsalBroadcastService,
--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -2,25 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Profile } from './profile';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProfileService {
 
-    constructor() { }
+    constructor(private httpClient: HttpClient) { }
 
     getProfile(): Observable<Profile> {
-        return new Observable<Profile>(observer => {
-            observer.next({
-                displayName: "John Doe",
-                givenName: "John",
-                id: "1",
-                jobTitle: "Software Engineer",
-                mail: "jdoe@azure.net",
-                surname: "Doe",
-                userPrincipalName: "jdoe@azure.net"
-            });
-        })
+        return this.httpClient.get<Profile>('https://graph.microsoft.com/v1.0/me');
     }
 }
